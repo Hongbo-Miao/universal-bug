9c25e7f9c2177f883c801070e04dd705b3a9b0c8
Hongbo Miao <Hongbo.Miao@outlook.com>
2016-09-18 22:10:17 -0400

    clean

--- src/app/components/+home/home.component.ts
+++ src/app/components/+home/home.component.ts
@@ -1,30 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
-  selector: 'h2c-home',
+  selector: 'home',
   template: `
-home
-    <!--<p class="lead">Hello <strong>{{name}}</strong>!</p>-->
+    home
   `
 })
 export class HomeComponent {
-  // subsName: Subscription;
-  // name: string = '';
-  //
-  // constructor(
-  //   private _store: Store<AppState>
-  // ) {}
-  //
-  // ngOnInit() {
-  //   this.subsName = this._store
-  //     .select(s => s.user && s.user.currentUser)
-  //     .filter(x => !!x)
-  //     .subscribe(currentUser => {
-  //       this.name = currentUser.name
-  //     });
-  // }
-  //
-  // ngOnDestroy() {
-  //   if (this.subsName) this.subsName.unsubscribe();
-  // }
+
 }
